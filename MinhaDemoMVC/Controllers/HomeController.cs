7278ab26169a598dda7232489e1ab968c27f3d8e
Daniel Sena <danielsena.yas@gmail.com>
2025-04-27 08:31:31 -0300

    02.05 - Entendendo o padrão MVC na prática

---
+++ MinhaDemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MinhaDemoMVC.Models;
+
+namespace MinhaDemoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string id, string categoria)
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
